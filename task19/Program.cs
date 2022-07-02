f0538844ad846bfc33e7afb9291c34c7f82a7ae2
SfShev <pshev1406@gmail.com>
2022-07-02 17:06:08 +0300

    Задача 19 "Является ли число палиндромом"

---
+++ task19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Если хотите узнать является ли пятизначное число палиндромом");
+Console.Write("Введите пятизначное число: ");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+
+string palindrome = Number.ToString();
+
+if (palindrome[0] == palindrome[4] && palindrome[1] == palindrome[3])
+{
+    Console.Write($"Число {Number} является палиндромом ");
+}
+if (palindrome[0] != palindrome[4])
+{
+
+    Console.Write($"Число {Number} не является палиндромом ");
+}
