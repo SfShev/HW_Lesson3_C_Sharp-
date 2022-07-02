08043096aac0d097bd03bd9e50c7fb51e5b4daec
SfShev <pshev1406@gmail.com>
2022-07-02 17:36:55 +0300

    Находит кубы чисел от 1 до N и -N до -1

---
+++ task23/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int j = -1;
+
+while (i <= number1)
+{
+    Console.Write($"{i * i * i}, ");
+    i++;
+}
+
+
+while (j >= number1)
+{
+    Console.Write($"{j * j * j}, ");
+    j -= 1;
+}
+Console.Write("\b \b");
+Console.Write("\b \b");
